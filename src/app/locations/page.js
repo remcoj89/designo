a863da2e14b42cbedc953c2713e615fc14efa791
remco jonk <remcojonk@hotmail.com>
2024-10-10 17:44:18 +0200

    css reset, css variables, css base layout and created pages

--- src/app/locations/page.js
+++ src/app/locations/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Location() {
   return (
     <div className="base-layout">
-      hello world
+      Location
     </div>
   );
 }
